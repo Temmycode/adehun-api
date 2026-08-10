e730a00081463f8558a24c48f8b5b8de2e26f692
Temiloluwa Akisanya <tolutech2004@gmail.com>
2026-08-10 14:35:03 +0100

    Fixing email crash issue

--- app/service/email_service.py
+++ app/service/email_service.py
@@ -1,8 +1,10 @@
 import resend
 
 from ..config import settings
+from ..logging import get_logger
 
 resend.api_key = settings.resend_api_key
+logger = get_logger(__name__)
 
 
 def send_invitation_email(email: str, invitation_link: str):
@@ -10,11 +12,18 @@ def send_invitation_email(email: str, invitation_link: str):
     Sends an invitation email using Resend.
     """
 
-    resend.Emails.send(
-        {
-            "from": "tolutech2004@gsmail.com",
-            "to": email,
-            "subject": "You've been invited to join Adehun",
-            "html": f"<p>You've been invited to join Adehun. Click <a href='{invitation_link}'>here</a> to accept.</p>",
-        }
-    )
+    try:
+        resend.Emails.send(
+            {
+                "from": "tolutech2004@gsmail.com",
+                "to": email,
+                "subject": "You've been invited to join Adehun",
+                "html": f"<p>You've been invited to join Adehun. Click <a href='{invitation_link}'>here</a> to accept.</p>",
+            }
+        )
+    except Exception as exc:
+        logger.exception(
+            "failed to send invitation email",
+            extra={"email": email, "invitation_link": invitation_link},
+            exc_info=exc,
+        )
